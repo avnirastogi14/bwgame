63d73c5292fc452c1d91b1423116f5fc0c213e2d
Avni <avnirastogi363@gmail.com>
2025-07-06 14:12:22 +0530

    space_style

--- src/components/MovieDisplay.js
+++ src/components/MovieDisplay.js
@@ -1,13 +1,30 @@
+import './MovieDisplay.css';
+
 function MovieDisplay({ movie, vowels, guessed }) {
   return (
-    <div className="movie-display">
-      {movie.split('').map((char, idx) => {
-        if (char === ' ') {
-          return <span key={idx} className="word-break">   </span>;
-        }
-        const displayChar = (vowels.includes(char) || guessed.includes(char)) ? char : '_';
-        return <span key={idx} className="letter">{displayChar}</span>;
-      })}
+    <div className="movie-display-container">
+      <div className="movie-display">
+        {movie.split('').map((char, idx) => {
+          if (char === ' ') {
+            return <span key={idx} className="word-break"></span>;
+          }
+          
+          const isRevealed = vowels.includes(char) || guessed.includes(char);
+          const isVowel = vowels.includes(char);
+          const isNumber = /[0-9]/.test(char);
+          
+          return (
+            <span 
+              key={idx} 
+              className={`letter ${isRevealed ? 'revealed' : 'hidden'} ${isVowel ? 'vowel' : ''} ${isNumber ? 'number' : ''}`}
+            >
+              {isRevealed ? char : ''}
+            </span>
+          );
+        })}
+      </div>
+      
+      <div className="movie-display-glow"></div>
     </div>
   );
 }
