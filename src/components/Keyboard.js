288b9aac3ffcf9f60dd67667b0fbac16d8cd0eb2
Avni <avnirastogi363@gmail.com>
2025-07-06 14:09:55 +0530

    numerics

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,7 +1,10 @@
-function Keyboard({ onGuess, guessed }) {
+function Keyboard({ onGuess, guessed, includeNumbers = true }) {
   const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  const numbers = '0123456789'.split('');
+
   return (
     <div style={{ marginTop: '20px' }}>
+      {/* Letters */}
       {letters.map((l) => (
         <button
           key={l}
@@ -12,6 +15,22 @@ function Keyboard({ onGuess, guessed }) {
           {l}
         </button>
       ))}
+      
+      {/* Numbers (if enabled) */}
+      {includeNumbers && (
+        <div style={{ marginTop: '10px' }}>
+          {numbers.map((n) => (
+            <button
+              key={n}
+              onClick={() => onGuess(n)}
+              className={guessed.includes(n) ? 'disabled' : ''}
+              disabled={guessed.includes(n)}
+            >
+              {n}
+            </button>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
