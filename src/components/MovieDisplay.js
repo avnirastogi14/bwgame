5a641427b645382d49d5cca591d52bbae9b7e8bd
Avni <avnirastogi363@gmail.com>
2025-07-07 23:19:40 +0530

    disp

--- src/components/MovieDisplay.js
+++ src/components/MovieDisplay.js
@@ -4,26 +4,26 @@ import './MovieDisplay.css';
 function MovieDisplay({ movie, vowels, guessed }) {
   return (
     <div className="movie-display">
-      {movie.split('').map((char, idx) => {
-        if (char === ' ') {
-          return <span key={idx} className="word-break"><br></br></span>;
-        }
+      {movie.split(' ').map((word, wordIdx) => (
+        <div className="movie-word" key={wordIdx}>
+          {word.split('').map((char, idx) => {
+            const isRevealed = vowels.includes(char) || guessed.includes(char);
+            const isVowel = vowels.includes(char);
+            const isNumber = /[0-9]/.test(char);
 
-        const isRevealed = vowels.includes(char) || guessed.includes(char);
-        const isVowel = vowels.includes(char);
-        const isNumber = /[0-9]/.test(char);
-
-        return (
-          <span
-            key={idx}
-            className={`letter ${isRevealed ? 'revealed' : 'hidden'} ${
-              isVowel && isRevealed ? 'vowel' : ''
-            } ${isNumber && isRevealed ? 'number' : ''}`}
-          >
-            {isRevealed ? char : '_'}
-          </span>
-        );
-      })}
+            return (
+              <span
+                key={idx}
+                className={`letter ${isRevealed ? 'revealed' : 'hidden'} ${
+                  isVowel && isRevealed ? 'vowel' : ''
+                } ${isNumber && isRevealed ? 'number' : ''}`}
+              >
+                {isRevealed ? char : '_'}
+              </span>
+            );
+          })}
+        </div>
+      ))}
     </div>
   );
 }
